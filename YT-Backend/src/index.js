4046e2cca7b084763d19f1dc929b70915fc5307e
Dileep01712 <dy3398214@gmail.com>
2024-07-20 19:04:49 +0530

    Utils Code Discusion

---
+++ YT-Backend/src/index.js
@@ -0,0 +1,51 @@
+// require('dotenv').config({path: './env'});
+import dotenv from "dotenv";
+import connectDB from './db/index.js';
+
+dotenv.config({
+    path: './env',
+});
+
+
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port: ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log("MongoDB connection failed!! ", err);
+    });
+
+
+
+
+
+
+
+
+/* EFI: First approach is to connect with database
+
+import mongoose from 'mongoose';
+import { DB_NAME } from './constants';
+import express from 'express';
+const app = express();
+
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on('error', (error) => {
+            console.log("App failed to connect with Database!!", error);
+            throw error;
+        });
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App listening on port ${process.env.PORT}`);
+        });
+
+    } catch (error) {
+        console.error("Mongoose failed to connect with Database!! ", error);
+        throw error;
+    }
+})()
+*/
